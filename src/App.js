af99c3a48829c95ca8d28d6c0ee81dee6d2886ec
Abdul Hamid <37768968+abdulhamid469@users.noreply.github.com>
2023-09-09 15:07:11 +0500

    Created file
    Create file and code it.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import TicTacToe from './components/TicTacToe/TicTacToe';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <TicTacToe />
     </div>
   );
 }
